01b82555c05ffb53f36f0f284b1282546edb9f92
Sunhanat-021-png <mixxa4790@gmail.com>
2026-07-01 14:37:40 +0700

    Hello.ts

---
+++ hello.ts
@@ -0,0 +1,21 @@
+function vat(price: number, type: string): number{
+    if(type==="E"){
+      return price*7/100;
+    }else if(type==="N"){
+      return 0;
+    }else{
+      return price*7/107;
+ }
+}
+let price1: number = 200;
+let price2: number = 750;
+console.log(`สินค้าราคา ${price1} บาท ภาษีมูบค่าเพิ่ม ${vat(price1, "N")} บาท`);
+console.log(`สินค้าราคา ${price2} บาท ภาษีมูบค่าเพิ่ม ${vat(price2, "I")} บาท`);
+
+function hello(name: string): string{
+    return `ยินต้อนรับ คุณ ${name} สู่เว็บไซต์ของ NPRU`;
+}
+
+console.log(hello("Sunhanat"));
+console.log(hello("Malithong"));
+console.log(hello("Mix"));
